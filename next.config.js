84dc0f7a71d34f5623e6fea5b1eb993cd174f9f8
eliel-gethash <100855259+eliel-gethash@users.noreply.github.com>
2022-04-04 18:05:03 -0300

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    NEXT_PUBLIC_SITE_NAME: process.env.NEXT_PUBLIC_SITE_NAME,
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Header, SEO } from "@/components";
+
+export default function Home() {
+  return (
+    <>
+      <SEO title="Teste" />
+      <Header />
+    </>
+  );
+}
