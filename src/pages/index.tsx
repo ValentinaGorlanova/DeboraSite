8e61189519d4686df9a092cd48f6e93aa800a51e
eliel-gethash <eliel.douna@gethash.com.br>
2022-04-13 18:11:05 -0300

    feat813: secao-instagram

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Begin } from "../components/Begin/Begin";
 import { Sobre } from "../components/Sobre/Sobre";
 import { Header } from "../components/Header/Header";
 import { Especialidade } from "../components/Especialidade/Especialidade";
+import { Instagram } from "../components/Instagram/Instagram";
 
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       <Begin />
       <Sobre />
       <Especialidade />
+      <Instagram/>
     </>
   )
 }
