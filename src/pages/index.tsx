5408c09bf25d767f7f18fd1ec8341338fc5eb929
unknown <matheuspedroso404@gmail.com>
2022-05-06 15:03:22 -0300

    feat:986 created section one for web version

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import { Header } from "../components/Header/Header";
+import { Header, SectionOne } from "../components";
 
 export default function Home() {
   return (
     <>
       <Header />
-      {/* <Begin />
-      <Sobre />
-      <Especialidade />
-      <Instagram />
-      <Consulta />
-      <Contato />
-      <img src="/whatsapp.svg" alt="" className="wha" />
+      <SectionOne />
+      {/* <Sobre /> */}
+      {/* <Especialidade /> */}
+      {/* <Instagram /> */}
+      {/* <Consulta /> */}
+      {/* <Contato /> */}
+      {/* <img src="/whatsapp.svg" alt="" className="wha" /> */}
 
-      <Footer /> */}
+      {/* <Footer />  */}
     </>
   );
 }
