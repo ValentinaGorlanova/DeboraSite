eda38b51d92fcf91f18daea485c17207ab505c0d
unknown <matheuspedroso404@gmail.com>
2022-05-09 12:20:43 -0300

    feat:989 created sectio WhenToLookn

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from "./SEO";
 export * from "./Header/Header";
 export * from "./Banner/Banner";
 export * from "./Symptoms/Symptoms";
+export * from "./WhenToLook";
+// export * from "./Sobre/Sobre";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Header, Banner, Symptoms } from "../components";
+import { Header, Banner, Symptoms, WhenToLook } from "../components";
 
 export default function Home() {
   return (
@@ -6,6 +6,7 @@ export default function Home() {
       <Header />
       <Banner />
       <Symptoms />
+      <WhenToLook />
       {/* <Sobre /> */}
       {/* <Especialidade /> */}
       {/* <Instagram /> */}
