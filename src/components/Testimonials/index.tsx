74c875df64a8f0a9c1af88c2a258fd95f5144127
unknown <matheuspedroso404@gmail.com>
2022-05-16 12:10:54 -0300

    feat:1006 adjust margin

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -7,7 +7,6 @@ export function Testimonials() {
     <div className={styles.container}>
       <div className={styles.content}>
         <h1>Depoimentos</h1>
-
         <div className={styles.cards}>
           {DataTestimonials && DataTestimonials.map((item) => <Card key={item.id} testimonial={item.testimonial} name={item.name} />)}
         </div>
