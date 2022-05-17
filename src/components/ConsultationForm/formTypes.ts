c4316d72d1150cafe78dd06e9c2043db7a53301e
unknown <matheuspedroso404@gmail.com>
2022-05-17 14:47:02 -0300

    feat:1005 created form functionality

---
+++ src/components/ConsultationForm/formTypes.ts
@@ -0,0 +1,22 @@
+export interface FormProps {
+  name: string;
+  email: string;
+  phone: string;
+  age: string;
+  date: string;
+  hour: string;
+  message: string;
+  presencial: string;
+  online: string;
+  option: string;
+  ansiedade: string;
+  depressao: string;
+  panico: string;
+  alcoolismo: string;
+  dependencia_quimica: string;
+  fobia_social: string;
+  dificuldades_sexuais: string;
+  autoconhecimento: string;
+  tdah: string;
+  outros: string;
+}
