d9eee7c4b033ef66459fd015d3176dbb0ec9370d
unknown <matheuspedroso404@gmail.com>
2022-05-31 16:21:00 -0300

    feat:1240 created login page

--- src/components/ConsultationForm/index.tsx
+++ src/components/ConsultationForm/index.tsx
@@ -4,7 +4,6 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import emailjs from "emailjs-com";
-
 import toast from "react-hot-toast";
 import styles from "./styles.module.scss";
 import { FormProps } from "./formTypes";
--- src/components/Pages/Login/index.tsx
+++ src/components/Pages/Login/index.tsx
@@ -1,5 +1,17 @@
+// import Link from "next/link";
+import { ModalLogin } from "./ModalLogin";
 import styles from "./styles.module.scss";
 
 export function Login() {
-  return <div className={styles.container}>Login</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imagesLeft}>
+        <img className={styles.img1} src="/imgLoginLeft1.png" alt="Figura ponto de interrogação" />
+        <img src="/imgLoginLeft.png" alt="Figura de mulher sentada em um sofá azul" />
+      </div>
+      <img className={styles.imageRight} src="/imgLoginRight.png" alt="Figura de homem sentado em um sofá azul" />
+
+      <ModalLogin />
+    </div>
+  );
 }
