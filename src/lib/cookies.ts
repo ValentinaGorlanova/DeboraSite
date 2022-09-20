aaa50e723015c617b20dcfc422f5fbddcafc604f
Edmar <edmar.filho@gethash.com.br>
2022-09-20 14:53:04 -0300

    feat: Logout user created

--- src/lib/cookies.ts
+++ src/lib/cookies.ts
@@ -6,10 +6,17 @@ const ONE_DAY = 24 * ONE_HOUR;
 const COOKIE_NAME = "@debora:auth.token";
 
 export function saveCookie(accessToken: string) {
-  nookies.set(null, COOKIE_NAME, accessToken, { maxAge: ONE_DAY });
+  nookies.set(null, COOKIE_NAME, accessToken, {
+    path: "/",
+    maxAge: ONE_DAY,
+  });
 }
 
 export function getContextCookie(context: GetServerSidePropsContext) {
   const cookie = nookies.get(context);
   return cookie[COOKIE_NAME];
+}
+
+export function logoutUser(context: GetServerSidePropsContext) {
+  nookies.destroy(context, COOKIE_NAME, { path: "/" });
 }
